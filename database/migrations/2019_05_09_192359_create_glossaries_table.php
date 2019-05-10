39c18aeb475063beffabf7e8a91d9ece8f5bdc97
Ezequiel Flores <ezequielsebastianflores@gmail.com>
2019-05-09 21:47:44 -0300

    create tables in database

---
+++ database/migrations/2019_05_09_192359_create_glossaries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGlossariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('glossaries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('language');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('glossarys');
+    }
+}
