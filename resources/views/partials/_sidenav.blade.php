69fb3ff58e547efe386bd27fb7e65d9e3a3bfe25
Ezequiel Flores <ezequielsebastianflores@gmail.com>
2019-05-09 21:50:55 -0300

    added links to sidebar

--- resources/views/partials/_sidenav.blade.php
+++ resources/views/partials/_sidenav.blade.php
@@ -23,15 +23,11 @@
         <!-- sidebar menu -->
         <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
             <div class="menu_section">
-                <h3>General</h3>
+                <h3>.</h3>
                 <ul class="nav side-menu">
-                    <li><a href="{{route('dashboard')}}"><i class="fa fa-home"></i> Dashboard</a>
+                    <li><a href="{{route('glossary.create')}}"><i class="fa fa-home"></i> New Glossary</a>
                     </li>
-                    <li><a><i class="fa fa-cogs"></i>Settings <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="#">Role Managment</a></li>
-                            <li><a href="#">Configuration</a></li>
-                        </ul>
+                    <li><a href="{{route('glossary.index')}}"><i class="fa fa-home"></i> Glossary list</a>
                     </li>
                 </ul>
             </div>
