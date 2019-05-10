a95e733e48a402734bd57728e7e8b0719138e4af
Ezequiel Flores <ezequielsebastianflores@gmail.com>
2019-05-09 21:49:40 -0300

    created views form term resource

---
+++ resources/views/term/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('title','New translation')
+@section('content')
+<div class="row">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+            <div class="x_content">
+                <form role="form" method="POST" action="{{ route('term.store', $glossary_id) }}">
+                {{csrf_field()}}
+                    <div class="row form-group">
+                        <div class="col-sm-6">                            
+                            <div class="form-group clearfix">
+                                <label class="control-label col-md-3 col-sm-3 col-xs-12">Word</label>
+                                <div class="col-md-9 col-sm-9 col-xs-12">
+                                    <input class="form-control" name="term" placeholder="Some Word..." type="text" autocomplete="off" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <button id="send" type="submit" class="btn btn-primary btn-lg" >Save Word </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/term/list_glossary_terms.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+@section('title','Terms in Glossary #'.$glossary_id)
+@section('content')
+<div class="row">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+            <div class="x_content">
+                <div class="row form-group">
+                  <div class="form-group clearfix">
+                        <a href={{route("term.create",$glossary_id)}} class="btn btn-info btn-md">Add Term</a>
+                        <a href={{route("glossary.index")}} class="btn btn-info btn-md pull-right">Go Back</a>
+                      </div>
+                    <div class="table-responsive col-sm-12">
+                        <table id="glossaries" class="table table-striped jambo_table">
+                            <thead>
+                              <tr class="headings">
+                                <th>Word</th>
+                                <th>See translations</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              @foreach ($terms as $term)
+                              <tr class="pointer">
+                                <td>{{$term['word']}}</td>
+                                <td><a href={{route("translation.index",$term["id"])}} class="btn btn-default btn-sm">View Translations</a><a href={{route("term.destroy",$term["id"])}} class="btn btn-default btn-sm">Delete Term</a></td>
+                              </tr>
+                              @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
