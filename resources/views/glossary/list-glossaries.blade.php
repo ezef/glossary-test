21263ad49fff68af74e8f404d2c56986bf671332
Ezequiel Flores <ezequielsebastianflores@gmail.com>
2019-05-23 19:57:52 -0300

    new glossary changed to modal window

--- resources/views/glossary/list-glossaries.blade.php
+++ resources/views/glossary/list-glossaries.blade.php
@@ -7,7 +7,7 @@
             <div class="x_content">
                 <div class="row form-group">
                   <div class="form-group clearfix">
-                        <a href={{route("glossary.create")}} class="btn btn-default btn-md">Add Glossary</a></td>
+                        <button type="button" class="btn btn-default btn-md pull-right" data-toggle="modal" data-target="#new-glossary">New glossary</button>
                       </div>
                     <div class="table-responsive col-sm-12">
                         <table id="glossaries" class="table table-striped jambo_table">
@@ -15,7 +15,7 @@
                               <tr class="headings">
                                 <th>Name</th>
                                 <th>Action</th>
-                              </tr>
+                              </tr> 
                             </thead>
                             <tbody>
                               @foreach ($glossaries as $glossary)
@@ -32,4 +32,72 @@
         </div>
     </div>
 </div>
+
+<form role="form" method="POST" action="{{ route('glossary.store') }}">
+{{csrf_field()}}    
+    <div id="new-glossary" class="modal fade" role="dialog">
+       <div class="modal-dialog modal-md">
+          <!-- Modal content-->
+          <div class="modal-content">
+             <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">new Glossary</h4>
+             </div>
+             <div class="modal-body">
+                  <div class="form-group clearfix">
+                      <label class="control-label col-md-3 col-sm-3 col-xs-12">Glossary Name</label>
+                      <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input class="form-control" name="name" placeholder="Name..." type="text" autocomplete="off" required>
+                      </div>
+                  </div>
+                  <div class="form-group clearfix">
+                      <label class="control-label col-md-3 col-sm-3 col-xs-12">Language</label>
+                      <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input class="form-control" name="lang" placeholder="Language of the Glossary..." type="text" autocomplete="off" required>
+                      </div>
+                  </div>
+                  <div class="form-group clearfix">
+                      <label class="control-label col-md-3 col-sm-3 col-xs-12">Term 1</label>
+                      <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input class="form-control" name="term[]"  type="text" autocomplete="off" required>
+                      </div>
+                  </div>                            
+                  <div class="form-group clearfix">
+                      <label class="control-label col-md-3 col-sm-3 col-xs-12">Term 2</label>
+                      <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input class="form-control" name="term[]"  type="text" autocomplete="off" required>
+                      </div>
+                  </div>                            
+                  <div class="form-group clearfix">
+                      <label class="control-label col-md-3 col-sm-3 col-xs-12">Term 3</label>
+                      <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input class="form-control" name="term[]" type="text" autocomplete="off" required>
+                      </div>
+                  </div>                            
+                  <div class="form-group clearfix">
+                      <label class="control-label col-md-3 col-sm-3 col-xs-12">Term 4</label>
+                      <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input class="form-control" name="term[]"  type="text" autocomplete="off" >
+                      </div>
+                  </div>                            
+                  <div class="form-group clearfix">
+                      <label class="control-label col-md-3 col-sm-3 col-xs-12">Term 5</label>
+                      <div class="col-md-9 col-sm-9 col-xs-12">
+                          <input class="form-control" name="term[]" type="text" autocomplete="off" >
+                      </div>
+                  </div>
+             </div>
+             <div class="modal-footer">
+                <div class="row">
+                   <div class="col-sm-6">
+                   </div>
+                   <div class="col-sm-6">
+                      <button type="submit" class="btn btn-primary btn-lg submit pull-right" >Create</button>
+                   </div>
+                </div>
+             </div>
+          </div>
+       </div>
+    </div>
+</form>
 @endsection
