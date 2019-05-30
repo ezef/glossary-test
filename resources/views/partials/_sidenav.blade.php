a098a730605c9d9275c53fa5aec9f06caa850566
Ezequiel Flores <ezequielsebastianflores@gmail.com>
2019-05-30 18:35:28 -0300

    some minor changes

--- resources/views/partials/_sidenav.blade.php
+++ resources/views/partials/_sidenav.blade.php
@@ -25,8 +25,6 @@
             <div class="menu_section">
                 <h3>.</h3>
                 <ul class="nav side-menu">
-                    <li><a href="{{route('glossary.create')}}"><i class="fa fa-home"></i> New Glossary</a>
-                    </li>
                     <li><a href="{{route('glossaries.index')}}"><i class="fa fa-home"></i> Glossary list</a>
                     </li>
                 </ul>
